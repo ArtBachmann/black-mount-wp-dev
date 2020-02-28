c15ed2d253eca0fb147156134b4161780c1fb1f1
Art Bachmann <bachmann.art@gmail.com>
2020-02-28 23:01:54 +0200

    added graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'WPGraphQL',
+        fieldName: 'wpgraphql',
+        url: 'http://test.artbachmann.fi/theblackmountpub',
+        // refetchInterval: 60,
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
